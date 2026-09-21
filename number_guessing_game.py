81c1c57487a65ba6cae9207761b815497a2b5fad
Theophilus Jacob <theophilusjacob@gmail.com>
2026-09-21 13:34:44 +0100

    Number Guessing Game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -8,11 +8,21 @@ Goodluck!!!
      """ )
 
 secret_number = random.randint(1, 20)
+count = 0
 
 for num in range(6): 
-
+    print("Guess my secret number.")
+    guess = int(input())
+    
+    if guess > secret_number:
+        print("Your guess is high, think of a lower number")
+    elif guess < secret_number: 
+        print("Your guess is low, the number is bigger")
+    else: 
+        break
+    count +1
 
 if guess == secret_number:
-    print("")
+    print(f"You are right! The number is {secret_number}\n You got it in {count} counts")
 else: 
-    print("")
+    print(f"You run out of guesses! The number is {secret_number}")  
