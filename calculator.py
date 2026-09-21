d44800a333d86bccb30c2a06c82572ea41433ab0
Theophilus Jacob <theophilusjacob@gmail.com>
2026-09-21 20:59:21 +0100

    Number Guessing Game

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("""
+        What do you want to do? 
+        1. Addition
+        2. Subtraction
+        3. Multiplication
+        4. Subtraction
+        X. Exit
+        """)
+
+    option = input("Choose an option: ")
+
+def addition(x,y):
+    return x + y
+def subtraction(x,y):
+    return x + y
+def multiplication(x,y):
+    return x * y
+def division(x,y):
+    return x / y 
