b412ec613e8b2ddf2e6dac76ce1292ebe032ced6
Theophilus Jacob <theophilusjacob@gmail.com>
2026-09-21 22:26:32 +0100

    Calculator

--- calculator.py
+++ calculator.py
@@ -5,10 +5,28 @@ def calculator():
         2. Subtraction
         3. Multiplication
         4. Subtraction
-        X. Exit
+        # X. Exit
         """)
 
     option = input("Choose an option: ")
+
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the first number: "))
+
+
+    if option == "1":
+        addition(first_number ,second_number)
+    elif option == "2":
+        subtraction(first_number ,second_number)
+    elif option == "3":
+        multiplication(first_number ,second_number)
+    elif option == "4":
+        division(first_number ,second_number)
+    # elif option == "X":
+    #     break 
+    else:
+        print("You have enter an invalid option")
+
 
 def addition(x,y):
     return x + y
